dff2f0726cf5306598c0842a5600b1e66163e65d
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-30 05:43:03 -0300

    Requisito 7: ok

--- script.js
+++ script.js
@@ -9,26 +9,17 @@ window.onload = function () {
   function geraCarta() {
     let palavra = inputCarta.value.split(' ');
     console.log(palavra);
-    for (let i = 0; i < palavra.length; i += 1) {
-      let span = document.createElement('span');
-      span.innerText = palavra[i];
-      pTextoGerado.appendChild(span);
-    }
-  }
-
-  //* requisito 4 foi automatico
-
-  //! Requisito 5
-
-  let verificarInput = document.querySelector('#criar-carta');
-  verificarInput.addEventListener('click', verificar);
-
-  function verificar() {
     if (inputCarta.value === '' || inputCarta.value === ' ') {
       pTextoGerado.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
       pTextoGerado.innerText = '';
-      geraCarta();
+      for (let i = 0; i < palavra.length; i += 1) {
+        let span = document.createElement('span');
+        span.innerText = palavra[i];
+        pTextoGerado.appendChild(span);
+      }
     }
   }
+
+  //* requisito 4 foi automatico
 };
